cf48d0157d260d5a059233f1b2c95193ece8b485
prarora98 <prateekisthebest98@gmail.com>
2019-09-17 20:54:03 +0530

    branch added

---
+++ button-design-system/button-design/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import Button from "./components/button";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ button-design-system/button-design/src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// function formatName(user) {
+//   return user.firstName + " " + user.lastName;
+// }
+// const getGreeting = user => {
+//   if (user) {
+//     return <h1>Hello, {formatName(user)}!</h1>;
+//   } else return <h1>Hello, Stranger.</h1>;
+// };
+// const user = {
+//   firstName: "Prateek",
+//   lastName: "Arora"
+// };
+
+// const element = getGreeting(user);
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, World</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById("root"));
+// }
+// setInterval(tick, 1000);
