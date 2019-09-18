e5a14a551efa63126b837003953ac38f3b13e04b
prarora98 <prateekisthebest98@gmail.com>
2019-09-18 11:20:58 +0530

    add: member card

--- tab/got-family/src/App.jsx
+++ tab/got-family/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import MemberCard from "./components/MemberCard";
 import "./App.css";
 
 export default function() {
   return (
     <React.Fragment>
       <Header />
+      <MemberCard />
     </React.Fragment>
   );
 }
---
+++ tab/got-family/src/components/MemberCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class MemberCard extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left"></div>
+            <div className="media-content"></div>
+          </div>
+          <div className="content">
+            <br />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MemberCard;
